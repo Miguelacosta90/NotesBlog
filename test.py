523de6a1e8e14d30fb5350f21ffb85ee9262b245
Miguelacosta90 <miguelacosta90@gmail.com>
2017-03-25 07:20:56 -0700

    new python code to learn about ooprogramming

--- test.py
+++ test.py
@@ -1,16 +1,22 @@
-value = ("Michael", "Instructor", "Coding Dojo")
-number = (3,7,9,12,26)
-(name, position, butt) = value
-for index, item in enumerate(number):
-    print(str(index)+" = "+str(item))
-print name
-print position
-print butt
-print len(value)
-print max(value)
-print min(value)
-print sum(number)
-print any(value)
-print all(value)
-print sorted(number)
-print tuple(reversed(number))
+class human(object):
+    def __init__(self, clan=None):
+        self.clan = clan
+        self.health = 100
+        self.strangth = 10
+        self.intelligence = 5
+        self.stealth = 5
+
+    def taunt(self):
+        print "this message is for Humans"
+
+miguel = human("Coding DOJO")
+miguel.taunt()
+
+class test(object):
+    def __init__(self, phrase='nothing was passed'):
+        print "this string will print from phrase" + phrase
+        self.phrase = phrase
+test1 = test("Hello, World")
+test2 = test()
+print "Test 1 has phrase: '" + test1.phrase + "'"
+print "Test 2 has phrase, '" + test2.phrase + "'"
